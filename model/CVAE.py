4abdbd50c40e2ad139d4f9d563dd81f98e584723
Christopher Hsu <christoherhsu8@gmail.com>
2019-11-21 22:18:09 -0500

    overhaul of model class, all layers must be initialized

--- model/CVAE.py
+++ model/CVAE.py
@@ -2,53 +2,65 @@ import torch
 import torch.utils.data
 from torch import nn, optim
 from torch.nn import functional as F
+import numpy as np
 import pdb
 
 #input is a tuple of images t and t+1 with lidar and control input
 class CVAE(nn.Module):
 	def __init__(self):
-		super(CVAE, self).__init__()
-		self.d = 0.4
+		super().__init__()
+		d = 0.4
 		self.z_size = 64
-		self.hidden = int(256)
-		#hidden must be > last layer conv enc layer or first dec layer
-		self.im_size = 4
-		self.shapes = {}
+		self.hidden = 256
+		ch_sz = 4
+		last_conv = 16
+		self.tensor = (2,last_conv,300,400)
+		flat = np.prod(self.tensor)
 
-	# channel_in, c_out, kernel_size, stride, padding
-	def convbn(self,ci,co,ksz,s=1,pz=0):
-		return nn.Sequential(
-			nn.Conv2d(ci,co,ksz,stride=s,padding=pz),
-			nn.ReLU(),
-			nn.BatchNorm2d(co))
+		# channel_in, c_out, kernel_size, stride, padding
+		def convbn(ci,co,ksz,s=1,pz=0):
+			return nn.Sequential(
+				nn.Conv2d(ci,co,ksz,stride=s,padding=pz),
+				nn.ReLU(),
+				nn.BatchNorm2d(co))
+		def mlp(in_size,hidden):
+			return nn.Sequential(
+				nn.Dropout(d),
+				nn.Linear(in_size,hidden),
+				nn.ReLU())
 
-	def mlp(self,in_size,hidden):
-		#Fully connected layer
-		return nn.Sequential(
-			nn.Dropout(self.d),
-			nn.Linear(in_size,hidden),
-			nn.ReLU())
+		#Encoder NN
+		self.enc = nn.Sequential(
+				nn.Dropout(d),
+				convbn(ch_sz,64,3,1,1),
+				convbn(64,32,1,1),
+				convbn(32,last_conv,1,1))
+		self.m1 = mlp(flat,self.hidden)
+		self.zmean = nn.Linear(self.hidden,self.z_size)
+		self.zstdev = nn.Linear(self.hidden,self.z_size)
+
+		#Decoder NN
+		self.expand_z = nn.Linear(self.z_size,self.hidden)
+		self.m2 = mlp(self.hidden,flat)
+		self.dec = nn.Sequential(
+				nn.Dropout(d),
+				convbn(last_conv,32,1,1),
+				convbn(32,64,1,1),
+				convbn(64,ch_sz,1,1))
 
 	def encoder(self, x):
-		enc = nn.Sequential(
-			nn.Dropout(self.d),
-			self.convbn(self.im_size,64,3,1,1),
-			self.convbn(64,32,1,1),
-			self.convbn(32,16,1,1))
-		
-		h_layer = torch.flatten(enc(x))	
+		h_layer = torch.flatten(self.enc(x))	
 		# Get shapes for decoder
-		self.shapes['tensor'] = enc(x).shape
-		self.shapes['len'] = len(h_layer)
+		# self.shapes['tensor'] = enc(x).shape
+		# self.shapes['len'] = len(h_layer)
 		# add control input in the following layer
-		m = self.mlp(self.shapes['len'],self.hidden)
-		h = m(h_layer)
+		h = self.m1(h_layer)
 		return h
 
 	def bottleneck(self, x):
-		bneck = nn.Linear(self.hidden,self.z_size)
-		z_mean = bneck(x)
-		z_stdev = bneck(x)
+		
+		z_mean = self.zmean(x)
+		z_stdev = self.zstdev(x)
 		#reparam to get z latent sample
 		std = torch.exp(0.5*z_stdev)
 		eps = torch.randn_like(std)
@@ -56,20 +68,12 @@ class CVAE(nn.Module):
 		return z, z_mean, z_stdev
 
 	def decoder(self, z):
-		m = nn.Linear(self.z_size,self.hidden)
-		m1 = self.mlp(self.hidden,self.shapes['len'])
-		dec = nn.Sequential(
-			nn.Dropout(self.d),
-			self.convbn(16,32,1,1),
-			self.convbn(32,64,1,1),
-			self.convbn(64,self.im_size,1,1))
-		#check where nonlinearities should go
-		h = m(z)
-		h1 = m1(h)
-		x = torch.reshape(h1,(self.shapes['tensor']))
+		#check the nonlinearities of this layer
+		h = self.expand_z(z)
+		h1 = self.m2(h)
 		#make sure to reshape data correctly
-		out = dec(x)
-		#maybe put a nonlinearity on the output dec
+		x = torch.reshape(h1,(self.tensor))
+		out = self.dec(x)
 		return out
 
 	def forward(self, x):
