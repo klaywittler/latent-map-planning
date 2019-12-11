8a0870544054ca31ce2e4f38286e31ca4ae3d429
Christopher Hsu <christoherhsu8@gmail.com>
2019-12-10 22:03:55 -0500

    final cvae model class

--- model/CVAE.py
+++ model/CVAE.py
@@ -7,14 +7,17 @@ import pdb
 
 #input is a tuple of images t and t+1 with lidar and control input
 class CVAE(nn.Module):
-	def __init__(self):
+	def __init__(self,batch=4):
 		super().__init__()
 		d = 0.4
 		self.z_size = 64
-		self.hidden = 256
-		ch_sz = 3
+		self.small = 256
+		self.hidden = 1024
+		ch_sz = 1
+		c1 = 64
+		c2 = 16
 		last_conv = 4
-		self.tensor = (1,last_conv,300,400)
+		self.tensor = (batch,last_conv,150,200)
 		flat = np.prod(self.tensor)
 
 		# channel_in, c_out, kernel_size, stride, padding
@@ -23,11 +26,15 @@ class CVAE(nn.Module):
 				nn.Conv2d(ci,co,ksz,stride=s,padding=pz),
 				nn.ReLU(),
 				nn.BatchNorm2d(co))
+		def decconvbn(ci,co,ksz,s=1,pz=0):		#ReLU nonlinearity
+			return nn.Sequential(
+				nn.ConvTranspose2d(ci,co,ksz,stride=s,padding=pz),
+				nn.ReLU(),
+				nn.BatchNorm2d(co))
 		def convlast(ci,co,ksz,s=1,pz=0):	#Sigmoid nonlinearity
 			return nn.Sequential(
-				nn.Conv2d(ci,co,ksz,stride=s,padding=pz),
-				nn.Sigmoid(),
-				nn.BatchNorm2d(co))
+				nn.ConvTranspose2d(ci,co,ksz,stride=s,padding=pz),
+				nn.Sigmoid())
 		def mlp(in_size,hidden):
 			return nn.Sequential(
 				nn.Dropout(d),
@@ -37,29 +44,33 @@ class CVAE(nn.Module):
 		#Encoder NN
 		self.enc = nn.Sequential(
 				nn.Dropout(d),
-				convbn(ch_sz,64,3,1,1),
-				convbn(64,16,1,1),
-				convbn(16,last_conv,1,1))
-		self.m1 = mlp(flat,self.hidden)
-		self.zmean = nn.Linear(self.hidden,self.z_size)
-		self.zstdev = nn.Linear(self.hidden,self.z_size)
+				convbn(ch_sz,c1,3,1,1),
+				convbn(c1,c2,3,1,1),
+				convbn(c2,last_conv,3,1,1))
+		# self.m1 = mlp(flat,self.hidden)
+		self.m1 = nn.Sequential(
+				nn.Dropout(d),
+				mlp(flat,self.hidden),
+				mlp(self.hidden, self.small))
+		self.zmean = nn.Linear(self.small,self.z_size)
+		self.zstdev = nn.Linear(self.small,self.z_size)
 
 		#Decoder NN
-		self.expand_z = nn.Linear(self.z_size,self.hidden)
-		self.m2 = mlp(self.hidden,flat)
+		self.expand_z = nn.Linear(self.z_size,self.small)
+		# self.m2 = mlp(self.hidden,flat)
+		self.m2 = nn.Sequential(
+				nn.Dropout(d),
+				mlp(self.small,self.hidden),
+				mlp(self.hidden,flat))
 		self.dec = nn.Sequential(
 				nn.Dropout(d),
-				convbn(last_conv,16,1,1),
-				convbn(16,64,1,1),
-				convlast(64,ch_sz,1,1))
+				decconvbn(last_conv,c2,3,1,1),
+				decconvbn(c2,c1,3,1,1),
+				convlast(c1,ch_sz,3,1,1))
 
 	def encoder(self, x):
-		h_layer = torch.flatten(self.enc(x))	
-		# Get shapes for decoder
-		# shapes1 = self.enc(x).shape
-		# shapes2 = len(h_layer)
-		# pdb.set_trace()
-		# add control input in the following layer
+		# h_layer = torch.flatten(self.enc(x))	
+		h_layer = self.enc(x).view(-1)
 		h = self.m1(h_layer)
 		return h
 
@@ -77,7 +88,8 @@ class CVAE(nn.Module):
 		h = self.expand_z(z)
 		h1 = self.m2(h)
 		#make sure to reshape data correctly
-		x = torch.reshape(h1,(self.tensor))
+		# x = torch.reshape(h1,(self.tensor))
+		x = h1.view(self.tensor)
 		out = self.dec(x)
 		return out
 
